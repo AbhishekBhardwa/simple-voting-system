49cec94a4c11a55fe607a7515c751dc3b77d4260
AbhishekBhardwa <95184333+AbhishekBhardwa@users.noreply.github.com>
2021-11-29 18:10:36 +0530

    Add files via upload

---
+++ simple-voting-system-1/README.md
@@ -0,0 +1,2 @@
+# simple-voting-system
+this is simplest vote system 
---
+++ simple-voting-system-1/routes/register.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Online voting system - Registratrion</title>
+        <link rel="stylesheet" href="../css/stylesheet.css">
+    </head>
+    <body>
+        <center>
+            <div id="headerSection">
+            <h1>Online Voting System</h1>  
+            </div>
+            <hr>
+
+            <h3>Registration</h3>
+                <form action="../api/register.php" method="POST" enctype="multipart/form-data">
+                    <input type="text" name="name" placeholder="Name" required>&nbsp
+                    <input type="number" name="mob" placeholder="Mobile" required><br><br>
+                    <input type="password" name="pass" placeholder="Password" required>&nbsp
+                    <input type="password" name="cpass" placeholder="Confirm Password" required><br><br>
+                    <input style="width: 31%" type="text" name="add" placeholder="Address" required><br><br>
+                    <div id="upload" style="width: 30%">
+                        Upload image: <input type="file" id="profile" name="image" required>
+                    </div><br>
+                    <div id="upload" style="width: 30%">
+                        Select your role:
+                        <select name="role">
+                            <option value="1">Voter</option>
+                            <option value="2">Group</option>
+                        </select><br>                   
+                    </div><br>
+                    <button id="loginbtn" type="submit" name="registerbtn">Register</button><br><br>
+                   <h2> Already user? <a href="../"><b>Login here<b></a></h2>
+                </form>
+            </center>
+    </body>
+</html>
